ce599bd9b72c0d576a3c95124aef1e0df7e4dae5
Bharath Ramachandraiah <r.bharath.ec@gmail.com>
2025-12-24 08:08:42 +0100

    Export Lightning module and data module from trainers package

--- padma/trainers/__init__.py
+++ padma/trainers/__init__.py
@@ -1,3 +1,5 @@
 from .trainer import Trainer
+from .lightning_module import ImageClassificationModule
+from .datamodule import ImageClassificationDataModule
 
-__all__ = ["Trainer"]
+__all__ = ["Trainer", "ImageClassificationModule", "ImageClassificationDataModule"]
